2e856087d5eeaf6b7e879df48e5e2c1fc839cb76
Cassia Nebel <162936759+cassianebel@users.noreply.github.com>
2024-06-17 18:00:26 -0500

    add courses and course routes/components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+
 import Header from './components/Header.jsx';
+import Courses from './components/Courses.jsx';
+import Course from './components/Course.jsx';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Courses />} />
+        <Route path="/courses/:id" element={<Course />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ client/src/components/Course.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Markdown from 'react-markdown';
+
+const Course = () => {
+  const [course, setCourse] = useState([]);
+  let { id} = useParams()
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/courses/${id}`)
+      .then(response => response.json())
+      .then(data => setCourse(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  return (
+    <main>
+      <div className="actions--bar">
+          <div className="wrap">
+              <a className="button" href={"/courses/" + id + "/update"}>Update Course</a>
+              <a className="button" href="#">Delete Course</a>
+              <a className="button button-secondary" href="/">Return to List</a>
+          </div>
+      </div>
+            
+      <div className="wrap">
+          <h2>Course Detail</h2>
+          <form>
+              <div className="main--flex">
+                  <div>
+                      <h3 className="course--detail--title">Course</h3>
+                      <h4 className="course--name">{course.title}</h4>
+                      <p>By </p>
+
+                      <Markdown>{course.description}</Markdown>
+                  </div>
+                  <div>
+                      <h3 className="course--detail--title">Estimated Time</h3>
+                      <p>{course.estimatedTime}</p>
+
+                      <h3 className="course--detail--title">Materials Needed</h3>
+                      <ul className="course--detail--list">
+                        <Markdown>{course.materialsNeeded}</Markdown>
+                      </ul>
+                  </div>
+              </div>
+          </form>
+      </div>
+    </main>
+  )
+}
+
+export default Course
