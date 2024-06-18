2bd6da2168091da64386c36b0052eaeeee839a5e
Cassia Nebel <162936759+cassianebel@users.noreply.github.com>
2024-06-17 21:27:16 -0500

    dynamic header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import { useContext } from "react";
+import UserContext from "../context/UserContext"
 
 const Header = () => {
+  const { authUser, actions } = useContext(UserContext);
+
   return (
     <header>
         <div className="wrap header--flex">
             <h1 className="header--logo"><a href="index.html">Courses</a></h1>
             <nav>
+            {authUser === null ?
                 <ul className="header--signedout">
                     <li><a href="/signup">Sign Up</a></li>
                     <li><a href="/signin">Sign In</a></li>
                 </ul>
+              :
+                <ul className="header--signedin">
+                    <li>Welcome, {authUser.firstName}!</li>
+                    <li><a href="/signout" onClick={actions.signOut}>Sign Out</a></li>
+                </ul>
+            }
             </nav>
         </div>
     </header>
